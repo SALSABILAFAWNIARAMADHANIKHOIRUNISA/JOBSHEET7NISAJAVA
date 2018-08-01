afb9e583754a681b2b8414ff8aa5769aed3e1a86
Hp <Hp@LAPTOP-TACEHB1Q>
2018-08-01 09:26:05 +0700

    Initial  Commit

---
+++ src/Latihan1.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hp
+ */
+public class Latihan1 {
+    public static void main(String[] args) {
+        String identitas = "nama lengkap / kelas / no";
+        System.out.println("Identitas : "+ identitas);
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if (a == 5)
+            System.out.println("nilai a sama dengan 5");
+            
+        System.out.println("\nIF.. 3");
+        if(a > 5) {
+            System.out.println("isi variable a : "+ a);
+            System.out.println("nilai a lebih dari 5");
+           
+        }
+        
+    }
+    
+}
