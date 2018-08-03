a9cf4d58e86784b4154087084eda9f0e12ff31b3
Hp <Hp@LAPTOP-TACEHB1Q>
2018-08-03 10:58:04 +0700

    Initial Commit

---
+++ src/Latihan/Latihan1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hp
+ */
+public class Latihan1 {
+    public static void main(String[] args) {
+        String identitas = "Salsabila Fawnia / XRPL5 / 40";
+        System.out.println("Identitas : "+ identitas);
+    }
+    
+}
