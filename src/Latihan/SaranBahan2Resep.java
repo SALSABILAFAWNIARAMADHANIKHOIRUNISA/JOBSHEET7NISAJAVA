d8d7730a6ec6d88ba3115759ca65af22ff4dc347
Hp <Hp@LAPTOP-TACEHB1Q>
2018-08-03 18:30:24 +0700

    Initial Commit.

---
+++ src/Latihan/SaranBahan2Resep.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Hp
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "Salsabila Fawnia Ramadhani Khoirunisa / XRPL5 / 40";
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
+    }
+    
+}
